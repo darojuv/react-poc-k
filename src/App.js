61e54b9949263aacf117f452b754d467cd9a5d25
darojuv-lacity <venugopalachary.daroju@lacity.org>
2025-07-05 12:16:57 -0700

    changed to basic

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import SpaceMissionDetails from "./screens/SpaceMissionDetails";
-import { StrictMode } from "react";
-
-import "./App.css";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <StrictMode>
-      <div className="app-bg">
-        <SpaceMissionDetails />
-      </div>
-    </StrictMode>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
