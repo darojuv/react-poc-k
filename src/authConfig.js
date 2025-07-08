990259a5f8dc9327d48fdc442eabff0a2fcbb0bf
Venu Daroju <darojuv@gmail.com>
2025-07-07 22:40:51 -0700

    Update authConfig.js

--- src/authConfig.js
+++ src/authConfig.js
@@ -3,7 +3,7 @@
       auth: {
         clientId: "c40312e1-e8f5-48b4-8396-31635de82be6",
         authority: "https://login.microsoftonline.com/4a402691-f5fe-4125-b95b-34f6db5c9ad1",
-        redirectUri: "http://localhost:3000, https://reactapp2-a3bhanbrcmhbdce0.canadacentral-01.azurewebsites.net", // or your app's redirect URI
+        redirectUri: "https://reactapp2-a3bhanbrcmhbdce0.canadacentral-01.azurewebsites.net", // or your app's redirect URI
       },
       cache: {
         cacheLocation: "localStorage", // or sessionStorage
